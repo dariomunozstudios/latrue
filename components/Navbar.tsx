f07273575f5a4fee8ec324819d33b4ce4f71629b
dariomunozstudios <146802847+dariomunozstudios@users.noreply.github.com>
2023-10-15 22:09:46 +0000

    Popover

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,6 @@
-import React from "react"
+import React, { Fragment } from "react"
 import { BsChatLeftHeartFill } from "react-icons/bs"
+import { MdPublish } from "react-icons/md"
 import { Popover, Transition } from '@headlessui/react'
 
 const join = [
@@ -18,17 +19,44 @@ const Navbar = () => {
         Latrue
         </div>
         
+        <div></div>
+        <Popover>
+
+        <Popover.Button>
         <button className="btn btn-primary">
         Join <BsChatLeftHeartFill/>
         </button>
+        </Popover.Button>
 
-        </div>
+<Transition as={Fragment}
+enter="transition ease-out duration-200"
+enterFrom="opacity-0 translate-y-1"
+enterTo="opacity-100 translate-y-0"
+leave="transition ease-in duration-150"
+leaveFrom="opacity-100 translate-y-0"
+leaveTo="opacity-0 translate-y-1">
+
+
+<Popover.Panel>
+
+
+
+
+</Popover.Panel>
+
+
+
+
+</Transition>
+</Popover>
+</div>
+</div>
     )
 }
 
 function PublishIcon(){
     return(
-        <></>
+        <MdPublish/>
     )
 }
 
