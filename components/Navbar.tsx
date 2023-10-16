f67c13880284cf70a5e57d805d7b77296dbca22a
dariomunozstudios <146802847+dariomunozstudios@users.noreply.github.com>
2023-10-16 05:04:30 +0000

    SimpleVersion

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,13 +4,6 @@ import { BsChatLeftHeartFill } from "react-icons/bs"
 import { MdPublish } from "react-icons/md"
 import { Popover, Transition } from '@headlessui/react'
 
-const join = [
-{
-name: 'Publish',
-href: '##',
-icon: PublishIcon,
-},
-]
 
 const Navbar = () => {
     return (
@@ -21,38 +14,16 @@ const Navbar = () => {
         </div>
         
         
-<Popover>
-    {({open}) => (
-
-<>
-
-<Popover.Button className={`
-${open ? '' : 'text-opacity-90'}
-group inline-flex items-center rounded-md bg-orange-700 px-3 py-2 text-base font-medium text-white hover:text-opacity-100 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75`}
->
 <div className="btn btn-primary">
     Join <BsChatLeftHeartFill/>
-</div>
-
-</Popover.Button>
-
-
-</>
-
-    )}
-
-</Popover>
 
 </div>
 
-    )
+</div>
+)
 }
 
-function PublishIcon(){
-    return(
-        <MdPublish/>
-    )
-}
+
 
 
 export default Navbar
