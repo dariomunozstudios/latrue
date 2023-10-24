32e6385b52d10126c153cd8f0b97adf62c66684b
dariomunozstudios <146802847+dariomunozstudios@users.noreply.github.com>
2023-10-24 03:14:50 +0000

    LoginPage

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,10 +1,14 @@
 "use client"
-
+import { Button } from "@/components/ui/button"
+import Link from "next/link"
 import { Menu } from "./Menu"
 
 const Navbar = () => {
     return (
 <div className="fixed top-0 p-4 flex justify-between z-10">
+<Button variant="outline">
+    <Link href={"/login"}>Login</Link>
+</Button>
 <Menu/>
 </div>
 )
