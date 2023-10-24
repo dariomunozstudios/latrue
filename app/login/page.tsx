42a48d722ac9dec83326243e46af0bf04a2cbacf
dariomunozstudios <146802847+dariomunozstudios@users.noreply.github.com>
2023-10-24 03:31:24 +0000

    LoginPush

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,15 +1,25 @@
 "use client"
 import React, { useEffect} from "react"
-import { app, provider, auth } from "@/firebase"
+import { provider, auth } from "@/firebase"
 import { Button } from "@/components/ui/button"
 import { useRouter } from "next/navigation"
 import { signInWithPopup, onAuthStateChanged } from "firebase/auth"
 
 
 export default function Login() {
+    const router = useRouter();
+
+    useEffect(()=>{
+onAuthStateChanged(auth, user =>{
+    if(user){
+        router.push("/")
+    }
+} )
+    },[])
+
     return (
         <div className="p-10">
-<Button variant="outline">LOGIN</Button>
+<Button variant="outline" onClick={()=>signInWithPopup(auth,provider)}>LOGIN</Button>
         </div>
     )
 }
